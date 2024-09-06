0ca3d02ad790504b2b4adffcffe2e160bfd9935a
Dmitry Romanov <this.romanovdmitry@gmail.com>
2024-09-06 16:20:44 +0300

    up Spring Boot to 3.0.0 switch Swagger to OpenApi

--- src/main/java/com/example/cxfmigration/SwaggerConfiguration.java
+++ src/main/java/com/example/cxfmigration/SwaggerConfiguration.java
@@ -1,25 +1,40 @@
 package com.example.cxfmigration;
 
-import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
+import org.apache.cxf.jaxrs.openapi.OpenApiFeature;
+import org.apache.cxf.jaxrs.swagger.ui.SwaggerUiConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfiguration {
 
+//    @Bean
+//    public Server jaxRsServer(Bus bus) {
+//        final JAXRSServerFactoryBean factory = new JAXRSServerFactoryBean();
+//
+//        factory.setServiceBean(userController);
+//        factory.setProvider(new JacksonJsonProvider());
+//        factory.setFeatures(Arrays.asList(swaggerFeature()));
+//        factory.setBus(bus);
+//        factory.setAddress("/");
+//        return factory.create();
+//    }
+
+
     @Bean
-    public Swagger2Feature swaggerFeature() {
-        Swagger2Feature swagger = new Swagger2Feature();
+    public OpenApiFeature swaggerFeature() {
+        OpenApiFeature swagger = new OpenApiFeature();
         swagger.setVersion("1.0.0");
-        swagger.setBasePath("/");
+        //swagger.setBasePath("/");
         swagger.setPrettyPrint(true);
-        swagger.setSchemes(new String[]{"https", "http"});
-        swagger.setContact("example@mail.ru");
+        //swagger.setSchemes(new String[]{"https", "http"});
+        swagger.setContactEmail("example@mail.ru");
         swagger.setDescription("Swagger cxf app");
         swagger.setTitle("Title");
         swagger.setSupportSwaggerUi(true);
-        swagger.setHost("localhost:8080");
-        System.out.println("http://localhost:8080/api-docs?url=/swagger.json");
+        swagger.setSwaggerUiConfig(new SwaggerUiConfig().configUrl("/openapi.json"));
+        //swagger.setHost("localhost:8080");
+        System.out.println("http://localhost:8080/api-docs?url=/openapi.json");
         return swagger;
     }
 
--- src/main/java/com/example/cxfmigration/api/UserController.java
+++ src/main/java/com/example/cxfmigration/api/UserController.java
@@ -3,11 +3,11 @@ package com.example.cxfmigration.api;
 import com.example.cxfmigration.model.User;
 import io.swagger.annotations.*;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/users")
